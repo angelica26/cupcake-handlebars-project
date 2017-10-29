ac1de6dc9defb0c7422222ebb5ea54d8477a79b6
Amie Morosco <aamorosco@gmail.com>
2017-10-29 13:19:55 -0400

    add JAWSDb

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,17 @@
 //Setting up MySQL connection
 var mysql = require("mysql");
-
-var connection = mysql.createConnection({
+var connection;
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
 	port: 3306,
 	host: "localhost",
 	user: "root",
 	password: "Starfire926!",
 	database: "cupcakes_db"
-});
-
+	});
+}
 connection.connect(function(err) {
 	if (err) {
 		console.log("Error connecting " + err.stack);
